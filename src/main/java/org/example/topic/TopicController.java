c5cbe090562f2c1fd0f8c0e1d8ff96d3e0cba730
karim rgaibi <karimrgaibi@gmail.com>
2025-09-19 13:40:37 +0200

    Creating a business service

--- src/main/java/org/example/topic/TopicController.java
+++ src/main/java/org/example/topic/TopicController.java
@@ -1,5 +1,6 @@
 package org.example.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +9,12 @@ import java.util.List;
 
 @RestController
 public class TopicController {
-    @RequestMapping("/topics")
-    public List<Topic> getAllTopivds() {
-        return Arrays.asList(
-                new Topic("spring", "Spring Framework", "Spring Framework Description"),
-                new Topic("java", "Core Java", "Core Java Description"),
-                new Topic("javascript", "Javascript", "Javascript Description")
 
-                );
+    @Autowired
+    private TopicService topicservice;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return topicservice.getAllTopics();
     }
 }
